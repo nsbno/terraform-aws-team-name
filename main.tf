0de8f495fc06374358165708f8e53b0f01545091
Wictor Zhao <wictor_x@hotmail.com>
2024-09-10 10:35:07 +0200

    Add initial module setup

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+}
+
+resource "aws_ssm_parameter" "team_name" {
+  name = "__platform__/team_name_handle"
+
+  description = "The name of the team that owns this environment. Format is lowercase."
+  type        = "String"
+  value       = var.team_name
+
+  allowed_pattern = "^[a-zæøå-]+$"
+
+  lifecycle {
+    ignore_changes = [
+      name,
+      type,
+      value
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "team_name" {
+  description = "The name of the team that owns this environment. Format is lowercase."
+
+  type = string
+}
